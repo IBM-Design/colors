01f05faf162137c36f3293f1c44cd931d46713f9
Dave Clark <dave.clark@uk.ibm.com>
2016-01-29 12:48:46 +0000

    Add a file extension to template files per comment from mbarlock

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,7 @@
 
 /*--- requires --------------------------------------------------------------*/
 var gulp = require('gulp'),
+    rename = require('gulp-rename'),
     map = require('through2-map'),
     handlebars = require('handlebars'),
     colors = require('./source/colors.js');
@@ -15,10 +16,19 @@ function compile(context) {
   });
 }
 
+/*--- convert template file name to output file name ------------------------*/
+function untemplate(path) {
+  // a template file of the form AAAA.BBB.hbs produces an output file AAAA.BBB
+  var dot = path.basename.lastIndexOf('.');
+  path.extname = path.basename.substring(dot);
+  path.basename = path.basename.substring(0, dot);
+}
+
 /*--- build task ------------------------------------------------------------*/
 gulp.task('build', function() {
   return gulp.src('./source/templates/*')
     .pipe(compile({ colors: colors }))
+    .pipe(rename(untemplate))
     .pipe(gulp.dest('./dist'));
 });
 
