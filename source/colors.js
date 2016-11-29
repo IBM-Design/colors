53373b14c9f8bb02093b9acb3203e1bceda3eb44
James Y. Rauhut <jamesrauhut@us.ibm.com>
2016-11-29 15:04:58 -0600

    Colors update for v2.0.0beta.2

--- source/colors.js
+++ source/colors.js
@@ -3,7 +3,7 @@
 // in a form suitable for driving templates to generate useful output files
 
 module.exports = {
-  version: '2.0.0-beta.1',
+  version: '2.0.0-beta.2',
   palettes: [
 
     // each entry in this array is an object with the following properties:
@@ -15,98 +15,100 @@ module.exports = {
     //      * value: the color as a six-digit hex value; eg, '#acefed'
 
     {
-      name: 'cool blue',
+      name: 'ultramarine',
+      synonyms: 'cool blue',
       values: [
-        { tone: 'core', value: '#2a51b8' },
-        { tone: '0', value: '#e5e9ff' },
-        { tone: '10', value: '#cdd7ff' },
-        { tone: '20', value: '#abbdff' },
-        { tone: '30', value: '#84a2ff' },
+        { tone: 'core', value: '#3151b7' },
+        { tone: '1', value: '#e7e9f7' },
+        { tone: '10', value: '#d1d7f4' },
+        { tone: '20', value: '#b0bef3' },
+        { tone: '30', value: '#89a2f6' },
         { tone: '40', value: '#648fff' },
         { tone: '50', value: '#3c6df0' },
-        { tone: '60', value: '#2a51b8' },
-        { tone: '70', value: '#254190' },
-        { tone: '80', value: '#203065' },
-        { tone: '90', value: '#1b2449' }
+        { tone: '60', value: '#3151b7' },
+        { tone: '70', value: '#2e3f8f' },
+        { tone: '80', value: '#252e6a' },
+        { tone: '90', value: '#20214f' }
       ]
     },
     {
       name: 'blue',
       values: [
-        { tone: 'core', value: '#1d79ca' },
-        { tone: '0', value: '#e2eafd' },
-        { tone: '10', value: '#c7d9fb' },
-        { tone: '20', value: '#a0c1f9' },
-        { tone: '30', value: '#63a8ff' },
-        { tone: '40', value: '#3c97f4' },
-        { tone: '50', value: '#1d79ca' },
-        { tone: '60', value: '#2c5a90' },
-        { tone: '70', value: '#224874' },
-        { tone: '80', value: '#1d3452' },
+        { tone: 'core', value: '#2d74da' },
+        { tone: '1', value: '#e1ebf7' },
+        { tone: '10', value: '#c8daf4' },
+        { tone: '20', value: '#a8c0f3' },
+        { tone: '30', value: '#79a6f6' },
+        { tone: '40', value: '#5392ff' },
+        { tone: '50', value: '#2d74da' },
+        { tone: '60', value: '#1f57a4' },
+        { tone: '70', value: '#25467a' },
+        { tone: '80', value: '#1d3458' },
         { tone: '90', value: '#19273c' }
       ]
     },
     {
-      name: 'warm blue',
+      name: 'cerulean',
+      synonyms: 'warm blue',
       values: [
         { tone: 'core', value: '#009de2' },
-        { tone: '0', value: '#dbecff' },
-        { tone: '10', value: '#badcff' },
-        { tone: '20', value: '#81c6ff' },
-        { tone: '30', value: '#3caef4' },
-        { tone: '40', value: '#009de2' },
-        { tone: '50', value: '#007eb6' },
-        { tone: '60', value: '#005e8a' },
-        { tone: '70', value: '#154a6b' },
-        { tone: '80', value: '#16364d' },
-        { tone: '90', value: '#142939' }
+        { tone: '1', value: '#deedf7' },
+        { tone: '10', value: '#c2dbf4' },
+        { tone: '20', value: '#95c4f3' },
+        { tone: '30', value: '#56acf2' },
+        { tone: '40', value: '#009bef' },
+        { tone: '50', value: '#047cc0' },
+        { tone: '60', value: '#175d8d' },
+        { tone: '70', value: '#1c496d' },
+        { tone: '80', value: '#1d364d' },
+        { tone: '90', value: '#1b2834' }
       ]
     },
     {
       name: 'aqua',
       values: [
-        { tone: 'core', value: '#00b7c0' },
-        { tone: '0', value: '#b6f5fb' },
-        { tone: '10', value: '#6aebf4' },
-        { tone: '20', value: '#42d2db' },
-        { tone: '30', value: '#00b7c0' },
-        { tone: '40', value: '#00a4ac' },
-        { tone: '50', value: '#00838a' },
-        { tone: '60', value: '#006368' },
-        { tone: '70', value: '#134e52' },
-        { tone: '80', value: '#14393b' },
-        { tone: '90', value: '#122a2c' }
+        { tone: 'core', value: '#00b6cb' },
+        { tone: '1', value: '#d1f0f7' },
+        { tone: '10', value: '#a0e3f0' },
+        { tone: '20', value: '#71cddd' },
+        { tone: '30', value: '#00b6cb' },
+        { tone: '40', value: '#12a3b4' },
+        { tone: '50', value: '#188291' },
+        { tone: '60', value: '#17616b' },
+        { tone: '70', value: '#164d56' },
+        { tone: '80', value: '#13393e' },
+        { tone: '90', value: '#122a2e' }
       ]
     },
     {
       name: 'teal',
       values: [
-        { tone: 'core', value: '#40d5bb' },
-        { tone: '0', value: '#acf9e7' },
-        { tone: '10', value: '#67eed3' },
-        { tone: '20', value: '#40d5bb' },
-        { tone: '30', value: '#00baa1' },
-        { tone: '40', value: '#00a78f' },
+        { tone: 'core', value: '#00A78F' },
+        { tone: '1', value: '#C0F5E8' },
+        { tone: '10', value: '#8EE9D4' },
+        { tone: '20', value: '#40D5BB' },
+        { tone: '30', value: '#00BAA1' },
+        { tone: '40', value: '#00A78F' },
         { tone: '50', value: '#008673' },
         { tone: '60', value: '#006456' },
-        { tone: '70', value: '#124f44' },
-        { tone: '80', value: '#133a32' },
-        { tone: '90', value: '#122b26' }
+        { tone: '70', value: '#124F44' },
+        { tone: '80', value: '#133A32' },
+        { tone: '90', value: '#122B26' }
       ]
     },
     {
       name: 'green',
       values: [
-        { tone: 'core', value: '#00be6a' },
-        { tone: '0', value: '#aefbc8' },
-        { tone: '10', value: '#6ff1a2' },
-        { tone: '20', value: '#3dd983' },
-        { tone: '30', value: '#00be6a' },
+        { tone: 'core', value: '#34bc6e' },
+        { tone: '1', value: '#cef3d1' },
+        { tone: '10', value: '#89eda0' },
+        { tone: '20', value: '#57d785' },
+        { tone: '30', value: '#34bc6e' },
         { tone: '40', value: '#00aa5e' },
-        { tone: '50', value: '#00884a' },
-        { tone: '60', value: '#006737' },
-        { tone: '70', value: '#11512e' },
-        { tone: '80', value: '#123b23' },
+        { tone: '50', value: '#00884b' },
+        { tone: '60', value: '#116639' },
+        { tone: '70', value: '#12512e' },
+        { tone: '80', value: '#123b22' },
         { tone: '90', value: '#112c1b' }
       ]
     },
@@ -114,7 +116,7 @@ module.exports = {
       name: 'lime',
       values: [
         { tone: 'core', value: '#95d13c' },
-        { tone: '0', value: '#cdf8a1' },
+        { tone: '1', value: '#d7f4bd' },
         { tone: '10', value: '#b4e876' },
         { tone: '20', value: '#95d13c' },
         { tone: '30', value: '#81b532' },
@@ -130,107 +132,107 @@ module.exports = {
       name: 'yellow',
       values: [
         { tone: 'core', value: '#fed500' },
-        { tone: '0', value: '#ffe8af' },
+        { tone: '1', value: '#fbeaae' },
         { tone: '10', value: '#fed500' },
-        { tone: '20', value: '#e2bd00' },
-        { tone: '30', value: '#c4a300' },
-        { tone: '40', value: '#af9100' },
-        { tone: '50', value: '#8c7400' },
-        { tone: '60', value: '#685717' },
-        { tone: '70', value: '#52451b' },
-        { tone: '80', value: '#3c3318' },
-        { tone: '90', value: '#2d2615' }
+        { tone: '20', value: '#e3bc13' },
+        { tone: '30', value: '#c6a21a' },
+        { tone: '40', value: '#b3901f' },
+        { tone: '50', value: '#91721f' },
+        { tone: '60', value: '#70541b' },
+        { tone: '70', value: '#5b421a' },
+        { tone: '80', value: '#452f18' },
+        { tone: '90', value: '#372118' }
+      ]
+    },
+    {
+      name: 'gold',
+      values: [
+        { tone: 'core', value: '#ffb000' },
+        { tone: '1', value: '#f5e8db' },
+        { tone: '10', value: '#ffd191' },
+        { tone: '20', value: '#ffb000' },
+        { tone: '30', value: '#e39d14' },
+        { tone: '40', value: '#c4881c' },
+        { tone: '50', value: '#9c6d1e' },
+        { tone: '60', value: '#74521b' },
+        { tone: '70', value: '#5b421c' },
+        { tone: '80', value: '#42301b' },
+        { tone: '90', value: '#2f261c' }
       ]
     },
     {
       name: 'peach',
       values: [
-        { tone: 'core', value: '#ffae55' },
-        { tone: '0', value: '#ffe5d5' },
-        { tone: '10', value: '#ffcfaa' },
-        { tone: '20', value: '#ffae55' },
-        { tone: '30', value: '#f58b00' },
-        { tone: '40', value: '#dc7c00' },
-        { tone: '50', value: '#ad6422' },
-        { tone: '60', value: '#834b17' },
-        { tone: '70', value: '#663c1b' },
-        { tone: '80', value: '#4a2d18' },
-        { tone: '90', value: '#372215' }
+        { tone: 'core', value: '#fe8500' },
+        { tone: '1', value: '#f5e8de' },
+        { tone: '10', value: '#fdcfad' },
+        { tone: '20', value: '#fcaf6d' },
+        { tone: '30', value: '#fe8500' },
+        { tone: '40', value: '#db7c00' },
+        { tone: '50', value: '#ad6418' },
+        { tone: '60', value: '#814b19' },
+        { tone: '70', value: '#653d1b' },
+        { tone: '80', value: '#482e1a' },
+        { tone: '90', value: '#33241c' }
       ]
     },
     {
       name: 'orange',
       values: [
-        { tone: 'core', value: '#ff8254' },
-        { tone: '0', value: '#fde5e0' },
-        { tone: '10', value: '#fccec5' },
-        { tone: '20', value: '#faad9a' },
-        { tone: '30', value: '#ff8254' },
-        { tone: '40', value: '#f76700' },
-        { tone: '50', value: '#c25622' },
-        { tone: '60', value: '#8f4224' },
-        { tone: '70', value: '#73341b' },
-        { tone: '80', value: '#532718' },
-        { tone: '90', value: '#3d1e15' }
-      ]
-    },
-    {
-      name: 'coral',
-      values: [
-        { tone: 'core', value: '#ff5e38' },
-        { tone: '0', value: '#fde5e3' },
-        { tone: '10', value: '#fccec9' },
-        { tone: '20', value: '#faaca3' },
-        { tone: '30', value: '#ff806d' },
-        { tone: '40', value: '#ff5e38' },
-        { tone: '50', value: '#d14722' },
-        { tone: '60', value: '#9a3924' },
-        { tone: '70', value: '#773023' },
-        { tone: '80', value: '#56251d' },
-        { tone: '90', value: '#3f1d18' }
+        { tone: 'core', value: '#fe6100' },
+        { tone: '1', value: '#f7e7e2' },
+        { tone: '10', value: '#f8d0c3' },
+        { tone: '20', value: '#faad96' },
+        { tone: '30', value: '#fc835c' },
+        { tone: '40', value: '#fe6100' },
+        { tone: '50', value: '#c45433' },
+        { tone: '60', value: '#993a1d' },
+        { tone: '70', value: '#782f1c' },
+        { tone: '80', value: '#56251a' },
+        { tone: '90', value: '#3a201b' }
       ]
     },
     {
       name: 'red',
       values: [
-        { tone: 'core', value: '#df3423' },
-        { tone: '0', value: '#fde5e4' },
-        { tone: '10', value: '#fccdcc' },
-        { tone: '20', value: '#faaba9' },
-        { tone: '30', value: '#f8837e' },
-        { tone: '40', value: '#ff5c52' },
-        { tone: '50', value: '#df3423' },
-        { tone: '60', value: '#ac200e' },
-        { tone: '70', value: '#84231b' },
-        { tone: '80', value: '#5f1c18' },
-        { tone: '90', value: '#461715' }
+        { tone: 'core', value: '#e62325' },
+        { tone: '1', value: '#f7e6e6' },
+        { tone: '10', value: '#fccec7' },
+        { tone: '20', value: '#ffaa9d' },
+        { tone: '30', value: '#ff806c' },
+        { tone: '40', value: '#ff5c49' },
+        { tone: '50', value: '#e62325' },
+        { tone: '60', value: '#aa231f' },
+        { tone: '70', value: '#83231e' },
+        { tone: '80', value: '#5c1f1b' },
+        { tone: '90', value: '#3e1d1b' }
       ]
     },
     {
       name: 'magenta',
       values: [
-        { tone: 'core', value: '#ff4ab3' },
-        { tone: '0', value: '#ffe3ef' },
-        { tone: '10', value: '#ffcae2' },
-        { tone: '20', value: '#ffa4d0' },
-        { tone: '30', value: '#ff76be' },
-        { tone: '40', value: '#ff4ab3' },
-        { tone: '50', value: '#d82793' },
-        { tone: '60', value: '#ab0072' },
-        { tone: '70', value: '#841459' },
-        { tone: '80', value: '#601540' },
-        { tone: '90', value: '#461330' }
+        { tone: 'core', value: '#ff509e' },
+        { tone: '1', value: '#f5e7eb' },
+        { tone: '10', value: '#f5cedb' },
+        { tone: '20', value: '#f7aac3' },
+        { tone: '30', value: '#f87eac' },
+        { tone: '40', value: '#ff509e' },
+        { tone: '50', value: '#dc267f' },
+        { tone: '60', value: '#a91560' },
+        { tone: '70', value: '#831b4c' },
+        { tone: '80', value: '#5d1a38' },
+        { tone: '90', value: '#401a29' }
       ]
     },
     {
       name: 'purple',
       values: [
         { tone: 'core', value: '#c22dd5' },
-        { tone: '0', value: '#f7e4fb' },
-        { tone: '10', value: '#f0cdf7' },
-        { tone: '20', value: '#e7aaf3' },
-        { tone: '30', value: '#de83ed' },
-        { tone: '40', value: '#d864e9' },
+        { tone: '1', value: '#f7e4fb' },
+        { tone: '10', value: '#efcef3' },
+        { tone: '20', value: '#e4adea' },
+        { tone: '30', value: '#d68adf' },
+        { tone: '40', value: '#cb71d7' },
         { tone: '50', value: '#c22dd5' },
         { tone: '60', value: '#9320a2' },
         { tone: '70', value: '#71237c' },
@@ -241,40 +243,40 @@ module.exports = {
     {
       name: 'violet',
       values: [
-        { tone: 'core', value: '#7D24C9' },
-        { tone: '0', value: '#EFE6FF' },
-        { tone: '10', value: '#E1D1FF' },
-        { tone: '20', value: '#D0B2FF' },
-        { tone: '30', value: '#BE8FFF' },
-        { tone: '40', value: '#B375FF' },
-        { tone: '50', value: '#A23DFF' },
-        { tone: '60', value: '#7D24C9' },
+        { tone: 'core', value: '#7d24c9' },
+        { tone: '1', value: '#ece8f5' },
+        { tone: '10', value: '#e2d2f4' },
+        { tone: '20', value: '#d2b5f0' },
+        { tone: '30', value: '#bf93eb' },
+        { tone: '40', value: '#b07ce8' },
+        { tone: '50', value: '#9753e1' },
+        { tone: '60', value: '#7732bb' },
         { tone: '70', value: '#602797' },
-        { tone: '80', value: '#44216A' },
-        { tone: '90', value: '#321C4C' }
+        { tone: '80', value: '#44216a' },
+        { tone: '90', value: '#321c4c' }
       ]
     },
     {
       name: 'indigo',
       values: [
-        { tone: 'core', value: '#3b33a8' },
-        { tone: '0', value: '#e9e8ff' },
-        { tone: '10', value: '#d6d4ff' },
-        { tone: '20', value: '#bbb9ff' },
-        { tone: '30', value: '#9f9bff' },
-        { tone: '40', value: '#8a85ff' },
-        { tone: '50', value: '#675eff' },
-        { tone: '60', value: '#4a40d5' },
-        { tone: '70', value: '#3b33a8' },
-        { tone: '80', value: '#2d2973' },
-        { tone: '90', value: '#232051' }
+        { tone: 'core', value: '#473793' },
+        { tone: '1', value: '#e9e8ff' },
+        { tone: '10', value: '#dcd4f7' },
+        { tone: '20', value: '#c7b6f7' },
+        { tone: '30', value: '#ae97f4' },
+        { tone: '40', value: '#9b82f3' },
+        { tone: '50', value: '#785ef0' },
+        { tone: '60', value: '#5a3ec8' },
+        { tone: '70', value: '#473793' },
+        { tone: '80', value: '#352969' },
+        { tone: '90', value: '#272149' }
       ]
     },
     {
       name: 'gray',
       values: [
-        { tone: 'core', value: '#949394' },
-        { tone: '0', value: '#eaeaea' },
+        { tone: 'core', value: '#777677' },
+        { tone: '1', value: '#eaeaea' },
         { tone: '10', value: '#d8d8d8' },
         { tone: '20', value: '#c0bfc0' },
         { tone: '30', value: '#a6a5a6' },
@@ -290,7 +292,7 @@ module.exports = {
       name: 'cool gray',
       values: [
         { tone: 'core', value: '#6f7878' },
-        { tone: '0', value: '#e3ecec' },
+        { tone: '1', value: '#e3ecec' },
         { tone: '10', value: '#d0dada' },
         { tone: '20', value: '#b8c1c1' },
         { tone: '30', value: '#9fa7a7' },
@@ -305,8 +307,8 @@ module.exports = {
     {
       name: 'warm gray',
       values: [
-        { tone: 'core', value: '#5f5757' },
-        { tone: '0', value: '#efe9e9' },
+        { tone: 'core', value: '#7d7373' },
+        { tone: '1', value: '#efe9e9' },
         { tone: '10', value: '#e2d5d5' },
         { tone: '20', value: '#ccbcbc' },
         { tone: '30', value: '#b4a1a1' },
@@ -321,43 +323,45 @@ module.exports = {
     {
       name: 'neutral-white',
       values: [
-        { tone: 'core', value: '#fdfdfd' },
-        { tone: '10', value: '#fdfdfd' },
-        { tone: '20', value: '#f9f9f9' },
-        { tone: '30', value: '#f4f4f4' },
-        { tone: '40', value: '#ececec' }
+        { tone: 'core', value: '#fcfcfc' },
+        { tone: '1', value: '#fcfcfc' },
+        { tone: '2', value: '#f9f9f9' },
+        { tone: '3', value: '#f6f6f6' },
+        { tone: '4', value: '#f3f3f3' }
       ]
     },
     {
       name: 'cool-white',
       values: [
         { tone: 'core', value: '#fbfcfc' },
-        { tone: '10', value: '#fbfcfc' },
-        { tone: '20', value: '#f9f9fb' },
-        { tone: '30', value: '#f0f2f4' },
-        { tone: '40', value: '#ecf0f2' }
+        { tone: '1', value: '#fbfcfc' },
+        { tone: '2', value: '#f8fafa' },
+        { tone: '3', value: '#f4f7f7' },
+        { tone: '4', value: '#f0f4f4' }
       ]
     },
     {
       name: 'warm-white',
       values: [
-        { tone: 'core', value: '#fbfbfb' },
-        { tone: '10', value: '#fbfbfb' },
-        { tone: '20', value: '#fdfbfb' },
-        { tone: '30', value: '#f7f5f5' },
-        { tone: '40', value: '#f2eeee' }
+        { tone: 'core', value: '#fdfcfc' },
+        { tone: '1', value: '#fdfcfc' },
+        { tone: '2', value: '#fbf8f8' },
+        { tone: '3', value: '#f9f6f6' },
+        { tone: '4', value: '#f6f3f3' }
       ]
     },
     {
       name: 'black',
       values: [
-        { tone: 'core', value: '#000' }
+        { tone: 'core', value: '#000' },
+        { tone: '100', value: '#000'}
       ]
     },
     {
       name: 'white',
       values: [
-        { tone: 'core', value: '#fff' }
+        { tone: 'core', value: '#fff' },
+        { tone: '0', value: '#fff'}
       ]
     }
   ]
